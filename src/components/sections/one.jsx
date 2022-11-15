68321723b2971e23707f75b2fe26441368019a59
Rushikesh Kardile <supersapiens@Orions-AirBook.local>
2022-11-15 16:51:38 +0530

    Minor UI Fixes & added new component

--- src/components/sections/one.jsx
+++ src/components/sections/one.jsx
@@ -54,7 +54,7 @@ function hero() {
               </p>
             </div>
           </div>
-          <div className="border bg-blur-pattern mt-20 mx-6 rounded-2xl h-max py-12 align-middle items-center col-span-2 md:col-span-1 flex bg-white bg-opacity-10">
+          <div className="border bg-blur-pattern mt-20 md:mx-6 rounded-2xl h-max py-12 align-middle items-center col-span-2 md:col-span-1 flex bg-white bg-opacity-10">
             <img src={shoe} className="mx-auto" />
           </div>
         </div>
