f27be140ebc65fd128e1f5ec22dfad643f239739
Rushikesh Kardile <supersapiens@Orions-AirBook.local>
2022-11-15 16:48:33 +0530

    Minor UI Fixes & added new component

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,21 +3,38 @@ import logo from "../../assets/images/kickto-logo-text.svg";
 import social from "../../assets/images/social.png";
 
 function footer() {
-  return (
-    <div className=" grid  sm:grid-cols-6 p-6 gap-3 text-white  align-middle items-center bg-orange-300 bg-opacity-10 py-6">
-        <div className="col-span-1 hidden md:block"></div>
-      <div className="col-span-1">
-        <img src={logo}  className="h-20 w-56"/>
-      </div>
-      <div className="col-span-1 flex-col">
-        <p className="font-semibold text-lg"> Contact us</p>
-        <p className="opacity-60">+91 123 456 7890</p>
-        <p className="opacity-60">hello@kickto.app</p>
-      </div>
-      <div className="">
-        <img src={social} className="h-14 w-52"/>    
-      </div>
-      <button className="btn bg-orange-500 text-white">Get Started</button>
+  return ( 
+    <div class=" bg-orange-700/10">
+        <div class="max-w-2xl mx-auto text-white py-10">
+            <div class="text-center">
+            <img src={logo} class=" px-28"/>
+                <p> Stay fit. All day, every day. </p>
+                <div class="flex justify-center my-10">
+                    <div class="flex items-center border w-auto rounded-lg px-4 py-2 w-52 mx-2">
+                        <img src="https://cdn-icons-png.flaticon.com/512/888/888857.png" class="w-7 md:w-8"/>
+                        <div class="text-left ml-3">
+                            <p class='text-xs text-gray-200'>Download on </p>
+                            <p class="text-sm md:text-base"> Google Play Store </p>
+                        </div>
+                    </div>
+                    <div class="flex items-center border w-auto rounded-lg px-4 py-2 w-44 mx-2">
+                        <img src="https://cdn-icons-png.flaticon.com/512/888/888841.png" class="w-7 md:w-8"/>
+                        <div class="text-left ml-3">
+                            <p class='text-xs text-gray-200'>Download on </p>
+                            <p class="text-sm md:text-base"> Apple Store </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="mt-28 flex flex-col md:flex-row md:justify-between items-center text-sm text-gray-400">
+                <p class="order-2 md:order-1 mt-8 md:mt-0"> &copy; Kickto, 2022</p>
+                <div class="order-1 md:order-2">
+                    <span class="px-2">About us</span>
+                    <span class="px-2 border-l">Contact us</span>
+                    <span class="px-2 border-l">Privacy Policy</span>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
--- src/components/sections/four.jsx
+++ src/components/sections/four.jsx
@@ -8,70 +8,103 @@ import shoe4 from "../../assets/images/shoe4.png";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import { Keyboard, Autoplay, Pagination, Navigation } from "swiper";
-import roadmap from "../../assets/vectors/roadmap.svg"
+import roadmap from "../../assets/vectors/roadmap.svg";
+import arrow from "../../assets/vectors/arrow.svg";
+
 function four() {
-    const carasoleData = [
-         { image: shoe2, altImg: "img1", role: "Runners", speed:"6-10 km/hr" },
-        { image: shoe3, altImg: "img1", role: "Gamers", speed:"2-6 km/hr"  },
-        { image: shoe4, altImg: "img1", role: "Joggers", speed:"6-10 km/hr"  },
-         ];
-     
+  const carasoleData = [
+    { image: shoe2, altImg: "img1", role: "Runners", speed: "6-10 km/hr" },
+    { image: shoe3, altImg: "img1", role: "Gamers", speed: "2-6 km/hr" },
+    { image: shoe4, altImg: "img1", role: "Joggers", speed: "6-10 km/hr" },
+  ];
+
   return (
     <div className=" min-h-screen w-full  text-white ">
-        <p className="text-5xl text-white font-semibold text-center mt-6 mb-4">Sneakers</p>
+      <p className="text-5xl text-white font-semibold text-left md:text-center mt-6 mb-4 mx-4">
+        Sneakers
+      </p>
       <Swiper
-            spaceBetween={50}
-            slidesPerView={5}
-            loop={true}
-            centeredSlides={true}
-            autoplay={{
-              delay: 3000,
-              disableOnInteraction: false,
-            }}
-            keyboard={{
-              enabled: true,
-            }}
-            breakpoints={{
-              "@0.00": {
-                slidesPerView: 1,
-                spaceBetween: 10,
-              },
-              "@0.75": {
-                slidesPerView: 1,
-                spaceBetween: 20,
-              },
-              "@1.00": {
-                slidesPerView: 3,
-                spaceBetween: 10,
-              },
-              "@1.50": {
-                slidesPerView: 4,
-                spaceBetween: 10,
-              },
-            }}
-            modules={[Keyboard, Autoplay, Pagination, Navigation]}
-            onSlideChange={() => console.log("slide change")}
-            onSwiper={(swiper) => console.log(swiper)}>
-            {carasoleData.map((data) => (
-              <SwiperSlide className='flex flex-col justify-center  h-56 w-56  p-4'>
-                <div className='flex flex-col justify-center items-center  h-full w-full bg-orange-400/10 border border-opacity-30 border-orange-400 rounded-xl p-8'>
-                  <img
-                    src={data.image}
-                    alt={data.altImg}
-                    className='object-center -rotate-12'
-                  /><div className="justify-between flex w-full align-middle  mt-12">
-                   <p className='text-white text-center font-bold text-2xl caption w-fit italic capitalize '>
-                    {data.role}
-                  </p>
-                  <p className="font-bold  align-middle  caption w-fit italic opacity-50 pt-2"> {data.speed}</p></div>
-                </div>
-              </SwiperSlide>
-            ))}{" "}
-          </Swiper>
+        spaceBetween={50}
+        slidesPerView={5}
+        loop={true}
+        centeredSlides={true}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        keyboard={{
+          enabled: true,
+        }}
+        breakpoints={{
+          "@0.00": {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+          "@0.75": {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          "@1.00": {
+            slidesPerView: 3,
+            spaceBetween: 10,
+          },
+          "@1.50": {
+            slidesPerView: 4,
+            spaceBetween: 10,
+          },
+        }}
+        modules={[Keyboard, Autoplay, Pagination, Navigation]}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        {carasoleData.map((data) => (
+          <SwiperSlide className="flex flex-col justify-center  h-56 w-56  p-4">
+            <div className="flex flex-col justify-center items-center  h-full w-full bg-orange-400/10 border border-opacity-30 border-orange-400 rounded-xl p-8">
+              <img
+                src={data.image}
+                alt={data.altImg}
+                className="object-center -rotate-12"
+              />
+              <div className="justify-between flex w-full align-middle  mt-12">
+                <p className="text-white text-center font-bold text-2xl caption w-fit italic capitalize ">
+                  {data.role}
+                </p>
+                <p className="font-bold  align-middle  caption w-fit italic opacity-50 pt-2">
+                  {" "}
+                  {data.speed}
+                </p>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}{" "}
+      </Swiper>
+      <p className="text-5xl text-white font-semibold text-left md:text-center mt-6 mb-4 mx-4">
+        Roadmap
+      </p>
 
-          <div>
-            <img src={roadmap}/>
-          </div>
+      <div className="w-full py-12">
+        <img src={roadmap} className="w-full" />
+      </div>
+
+<div className="my-8">
+      <p className="text-5xl text-white font-semibold text-left md:text-center mt-6 mb-4 mx-4">
+        Tokenomics
+      </p>
+      <div className="text-center items-center flex flex-col">
+        <div className="bg-orange-400/10 rounded-xl w-max  px-6 py-2  text-xl border border-orange-800/30">
+          In-app marketplace
+        </div>
+        <img src={arrow} className="fill-white h-24 my-4" />
+
+        <div className="bg-orange-400/10 rounded-xl w-max  px-6 py-2  text-xl border border-orange-800/30">
+          Move to earn{" "}
+        </div>
+        <img src={arrow} className="fill-white h-24 my-4" />
+
+        <div className="bg-orange-400/10 rounded-xl w-max  px-6 py-2  text-xl border border-orange-800/30">
+          In-app wallet / swap{" "}
+        </div>
+      </div></div>
     </div>
   );
 }
