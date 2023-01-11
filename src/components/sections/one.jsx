99a76e15b370528179d4d7b4e2d7a5be9f282f19
Rushikesh Kardile <supersapiens@Orions-AirBook.local>
2023-01-11 22:58:52 +0530

    iPAD UI Fix

--- src/components/sections/one.jsx
+++ src/components/sections/one.jsx
@@ -55,7 +55,7 @@ function hero() {
         <div className="grid grid-cols-3 px-4 hero-content">
           <div className="bg-primary/20 align-middle items-center   rounded-3xl mx-auto col-span-3 border border-primary   h-3/4  grid md:grid-cols-2 grid-cols-1">
             <div className="flex flex-col  mt-6 md:-mt-16  text-left">
-              <p className="text-6xl font-bold items-center text-center   text-primary">
+              <p className="text-6xl md:text-4xl lg:text-6xl font-bold items-center text-center   text-primary">
                 Coming Soon
               </p>
               <p className="text-md italic items-center text-center text-black/80 hidden">
