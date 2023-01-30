5ddf77e992ffa7b5f170b0b21b06acedc933d4f0
Rushikesh Kardile <supersapiens@Orions-AirBook.local>
2023-01-30 19:11:28 +0530

    Fixed Navbar Dropdown Color

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -127,7 +127,7 @@ function navbar() {
           </li>
         </ul>
       </div>
-      <div className="navbar-end md:hidden">
+      <div className="navbar-end md:hidden ">
         <div className="dropdown  dropdown-end">
           <button tabIndex={0} className="btn btn-square btn-ghost text-white">
             <svg
@@ -146,7 +146,7 @@ function navbar() {
           </button>
           <ul
             tabIndex={0}
-            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            className="menu menu-compact  dropdown-content mt-3 p-2 shadow bg-white rounded-box w-52"
           >
             <li>
               <a>Marketplace</a>
