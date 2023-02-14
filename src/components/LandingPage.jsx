7452dc14cb549f795eaaa323c75cdeb2f8b19482
Rushikesh Kardile <supersapiens@Orions-AirBook.local>
2023-02-15 00:52:32 +0530

    Added GTAG

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,4 +1,4 @@
-  import "@splidejs/react-splide/css";
+import "@splidejs/react-splide/css";
 import React from "react";
 import Navbar from "./navbar/Navbar";
 import Hero from "./hero/Hero";
@@ -7,7 +7,7 @@ import Two from "./sections/secTwo";
 import Three from "./sections/secThree";
 import Four from "./sections/secFour";
 import Five from "./sections/secFive";
-import Tokenomics from "../components/sections/tokenomics"
+import Tokenomics from "../components/sections/tokenomics";
 import FiveHalf from "./sections/secFiveHalf";
 import Six from "./sections/secSix";
 import SixHalf from "./sections/secSixHalf";
@@ -19,7 +19,8 @@ import { loadFull } from "tsparticles";
 import { useEffect } from "react";
 import AnimatedCursor from "react-animated-cursor";
 import Roadmap from "./sections/Roadmap-back";
-import Track from "./sections/Track.jsx"
+import Track from "./sections/Track.jsx";
+import Partners from "./sections/Partners";
 const LandingPage = () => {
   const [showElement, setShowElement] = useState(true);
   useEffect(() => {
@@ -34,7 +35,7 @@ const LandingPage = () => {
 
       <Hero />
       <One />
-
+      <Partners />
       <Two />
 
       <Three />
@@ -44,8 +45,8 @@ const LandingPage = () => {
       <Five />
       <FiveHalf />
       <Six />
-      <Track/>
-      <Tokenomics/>
+      <Track />
+      <Tokenomics />
       <Footer />
     </>
   );
